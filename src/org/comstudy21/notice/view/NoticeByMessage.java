bb13fcea29944e1853770e76acdc4d2850663502
yeonsu2977 <dustn2979@naver.com>
2021-12-28 19:39:07 +0900

    memo program

--- src/org/comstudy21/notice/view/NoticeByMessage.java
+++ src/org/comstudy21/notice/view/NoticeByMessage.java
@@ -5,8 +5,7 @@ import org.comstudy21.notice.model.NoticeDao;
 import org.comstudy21.notice.model.NoticeVo;
 
 public class NoticeByMessage extends NoticeView {
-//	NoticeDao dao = new NoticeDao();
-//	NoticeVo vo = new NoticeVo();
+
 
 	@Override
 	public void display() {
@@ -17,18 +16,6 @@ public class NoticeByMessage extends NoticeView {
 		R.noticeVo = new NoticeVo(0, null, message, null);
 		
 		
-//		
-//		for (int i = 0; i < message.length(); i++) {
-//			message.substring(i, message.length());
-//
-//		}
-		// controller로 이동
 	}
-	
-//	public static void main(String[] args) {
-//		String message = "kimjihyeon";
-//		String i = message.substring(0, message.length());
-//		System.out.println(i);
-//	}
 
 }
